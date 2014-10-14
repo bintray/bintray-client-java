e0e4210be5ba5d7a127772bcf575e09804aae3df
jbaruch <jbaruch@jfrog.com>
2014-10-14 13:47:46 +0300

    additional attributes of Version added

--- api/src/main/java/com/jfrog/bintray/client/api/details/VersionDetails.java
+++ api/src/main/java/com/jfrog/bintray/client/api/details/VersionDetails.java
@@ -10,6 +10,8 @@ public class VersionDetails {
     String description = null;
     DateTime released = null;
     String name;
+    String vcsTag;
+    GpgDetails gpgDetails;
 
     public VersionDetails(String name) {
         this.name = name;
@@ -23,5 +25,34 @@ public class VersionDetails {
     public VersionDetails released(DateTime released) {
         this.released = released;
         return this;
+    }
+
+    public VersionDetails gpgDetails(GpgDetails gpgDetails){
+        this.gpgDetails = gpgDetails;
+        return this;
+    }
+
+    public VersionDetails vcsTag(String vcsTag){
+        this.vcsTag = vcsTag;
+        return this;
+    }
+
+    public static class GpgDetails {
+        boolean sign;
+        String passphrase;
+
+        public GpgDetails(){
+
+        }
+
+        public GpgDetails sign(boolean sign){
+            this.sign = sign;
+            return this;
+        }
+
+        public GpgDetails passphrase(String passphrase){
+            this.passphrase = passphrase;
+            return this;
+        }
     }
 }
