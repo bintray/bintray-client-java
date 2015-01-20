fe598fa9c5859b00f5bb52a29909765077defd61
danf <danf@jfrog.com>
2015-01-20 20:20:57 +0200

    Delete Groovy implementations

--- api/src/main/java/com/jfrog/bintray/client/api/handle/ArtibutesSearchQuery.java
+++
@@ -1,9 +0,0 @@
-package com.jfrog.bintray.client.api.handle;
-
-/**
- * @author jbaruch
- * @since 13/11/13
- */
-public interface ArtibutesSearchQuery {
-    AttributesSearchQueryClause byAttributeName(String name);
-}
